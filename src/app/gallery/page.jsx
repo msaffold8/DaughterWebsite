097fc2fbc0efd3b50918aeabc0c859215a310b9f
Mel <melaquansaffold@yahoo.com>
2023-12-05 15:06:12 -0500

    New Image

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -2,12 +2,12 @@ import Image1 from './image1.jpg';
 import Image2 from './image2.jpg';
 import Image3 from './image3.jpg';
 import Image4 from './image4.jpg';
-import Image5 from './image6.jpg';
-import Image6 from './image7.jpg';
-import Image7 from './image8.jpg';
-import Image8 from './image9.jpg';
-import Image9 from './image10.jpg';
-import Image10 from './image11.jpg';
+import Image5 from './image5.jpg';
+import Image6 from './image6.jpg';
+import Image7 from './image7.jpg';
+import Image8 from './image8.jpg';
+import Image9 from './image9.jpg';
+import Image10 from './image10.jpg';
 import Image11 from './image11.jpg';
 
 
