841a76b0a85375dc1743305d0efeb0931bf4e962
Mel <melaquansaffold@yahoo.com>
2023-12-05 13:24:48 -0500

    To uppercase

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -20,7 +20,7 @@ import image1 from '@/images/photos/image-1.jpg'
 import image2 from '@/images/photos/image-2.jpg'
 import image3 from '@/images/photos/image-3.jpg'
 import image4 from '@/images/photos/image-4.jpg'
-import image5 from '@/images/photos/image-5.jpg'
+import image5 from '@/images/photos/Image-5.jpg'
 
 
 import { getAllArticles } from '@/lib/articles'
