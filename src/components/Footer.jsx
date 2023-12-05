fe8aed5ad5c30c50df90fed709b1815b5da7506d
Mel <melaquansaffold@yahoo.com>
2023-12-05 14:09:01 -0500

    Changed Navigation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,10 +21,10 @@ export function Footer() {
           <ContainerInner>
             <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
               <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
-                <NavLink href="/about">About</NavLink>
-                <NavLink href="/projects">Projects</NavLink>
-                <NavLink href="/speaking">Speaking</NavLink>
-                <NavLink href="/uses">Uses</NavLink>
+                <NavLink href="/about">About Me!</NavLink>
+                <NavLink href="/projects">My Projects</NavLink>
+                <NavLink href="/speaking">My Stories</NavLink>
+                <NavLink href="/uses">My Favorites </NavLink>
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
                 &copy; {new Date().getFullYear()} LaQueen Saffold. All rights
